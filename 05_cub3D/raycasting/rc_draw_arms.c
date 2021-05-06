5127338dbddbbbe034a2e897ecba0c3985978de6
tderwedu <tderwedu@student.s19.be>
2021-05-06 08:56:05 +0200

    cub3D:v0.80-ADD arms 80%

--- 05_cub3D/raycasting/rc_draw_arms.c
+++ 05_cub3D/raycasting/rc_draw_arms.c
@@ -6,7 +6,7 @@
 /*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/04 09:30:10 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/05/04 19:10:48 by tderwedu         ###   ########.fr       */
+/*   Updated: 2021/05/06 08:54:45 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,10 +64,11 @@ int	rc_get_arms(t_mlx *mlx)
 
 void	rc_draw_arms(t_mlx *mlx)
 {
-	double	ratio;
 	int		i;
+	double	ratio;
 	t_loop	box;
 
+	// Left Arm
 	box = (t_loop){0, 0, 0, mlx->left_arm[0].height, 0, 0, 0, mlx->left_arm[0].width};
 	ratio = 1.5 * box.y_tex_max / mlx->height;
 	box.y = mlx->height - box.y_tex_max * ratio;
@@ -77,9 +78,26 @@ void	rc_draw_arms(t_mlx *mlx)
 		set_tex_y_loop(mlx, &mlx->left_arm[0], &box, 0xFFFFFF);
 	else
 		set_tex_y_tex_loop(mlx, &mlx->left_arm[0], &box, 0xFFFFFF);
+	
+	// Left Arm
 	i = (mlx->attack > 0);
 	box = (t_loop){0, mlx->height, 0, mlx->right_arm[i].height, 0, 0, 0, mlx->right_arm[i].width};
 	ratio = 1.5 * box.y_tex_max / mlx->height;
+	if (mlx->attack == 1 || mlx->attack == 9)
+	{
+		box.x_tex_max /= 2;
+		box.y_tex_max /= 2;
+	}
+	else if (mlx->attack == 2 || mlx->attack == 8)
+	{
+		box.x_tex_max = (2 * box.x_tex_max ) / 3;
+		box.y_tex_max = (2 * box.y_tex_max ) / 3;
+	}
+	else if (mlx->attack == 3 || mlx->attack == 7)
+	{
+		box.x_tex_max = (3 * box.x_tex_max ) / 4;
+		box.y_tex_max = (3 * box.y_tex_max ) / 4;
+	}
 	box.y = mlx->height - box.y_tex_max * ratio;
 	box.x = mlx->width - box.x_tex_max * ratio;
 	box.x_max = box.x_tex_max * ratio;
@@ -88,8 +106,8 @@ void	rc_draw_arms(t_mlx *mlx)
 		set_tex_y_loop(mlx, &mlx->right_arm[i], &box, 0xFFFFFF);
 	else
 		set_tex_y_tex_loop(mlx, &mlx->right_arm[i], &box, 0xFFFFFF);
-	if (mlx->attack > 0)
+	if (mlx->attack > 0) //TOD: improve and ADD object detection
 		mlx->attack++;
-	if (mlx->attack > 5)
+	if (mlx->attack > 9)
 		mlx->attack = 0;
 }
