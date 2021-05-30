4f5dfb18e8411ae8ee27df36880e32f0a87421df
tderwedu <tderwedu@student.s19.be>
2021-05-30 12:49:21 +0200

    ft_server: input parsing 100%

--- 07_push_swap/include/push_swap.h
+++ 07_push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 10:36:57 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/05/29 11:20:16 by tderwedu         ###   ########.fr       */
+/*   Updated: 2021/05/30 12:48:00 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,39 @@
 # include <fcntl.h>
 # include <limits.h>
 # include "libft.h"
+# include <stdio.h> //TODO:remove
+
+# define	DEBUG		1
+
+typedef struct s_link
+{
+	int				val;
+	struct s_link	*next;
+	struct s_link	*prev;
+}				t_link;
+
+typedef struct	s_stk
+{
+	int		nb_a;
+	t_link	*stk_a;
+	int		nb_b;
+	t_link	*stk_b;
+}				t_stk;
+
+/*
+** parse_input.c
+*/
+
+int		parse_str(t_stk *stk, char *str);
+int		parse_input(int argc, char **argv, t_stk *stks);
+int		get_new_val(char **str, int *ptr);
+int		check_new_val(t_stk *stk, int new_val);
+int		add_new_val(t_stk *stk, int new_val);
+
+/*
+** utils.c
+*/
+
+void	init_stacks(t_stk *stk);
 
 #endif
