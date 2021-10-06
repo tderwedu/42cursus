c54282206521e0e1869a7e1f40e4a1f407a33b03
tderwedu <tderwedu@student.s19.be>
2021-10-06 09:42:23 +0200

    MiniShell:v0.90- merged with xps

--- 10_MiniShell/src/builtins/msh_cd.c
+++ 10_MiniShell/src/builtins/msh_cd.c
@@ -1,17 +1,13 @@
 /* ************************************************************************** */
-/*																			*/
-/*														:::	  ::::::::	*/
-<<<<<<< HEAD
-/*	MSG_CD.c											:+:	  :+:	:+:	*/
-=======
-/*	msh_cd.c											:+:	  :+:	:+:	*/
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
-/*													+:+ +:+		 +:+	 */
-/*	By: tderwedu <tderwedu@student.s19.be>		 +#+  +:+		+#+		*/
-/*												+#+#+#+#+#+	+#+			*/
-/*	Created: 2021/09/28 10:05:51 by tderwedu		  #+#	#+#			 */
-/*	Updated: 2021/10/01 14:25:24 by tderwedu		 ###	########.fr		*/
-/*																			*/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   msh_cd.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 09:33:38 by tderwedu          #+#    #+#             */
+/*   Updated: 2021/10/06 09:38:00 by tderwedu         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
@@ -25,7 +21,6 @@
 #include <sys/errno.h>
 #include "utils.h"
 
-<<<<<<< HEAD
 typedef struct stat		t_stat;
 
 //TODO: on single HEADER to rule them all
@@ -60,116 +55,6 @@ int	MSG_CD_export(t_msh *msh, char *name, char *val)
 		var[len_name + 1 + len_val] = '\0';
 		free(*env);
 		*env = var;
-=======
-# define DIRSEP		'/'
-typedef struct stat		t_stat;
-
-// #define ELOOP			62			  /* Too many levels of symbolic links */
-// #define ENAMETOOLONG	63			  /* File name too long */
-
-int	path_isabsolute(char *str)
-{
-  if (str == 0 || str[0] == '\0')
-	return (0);
-  if (str[0] == '/')
-	return (1);
-  if (str[0] == '.' && (str[1] == '/' || str[1] == '\0'))
-	return (1);
-  if (str[0] == '.' && str[1] == '.' && (str[2] == '/' || str[2] == '\0'))
-	return (1);
-  return (0);
-}
-
-/*
-** Return 1 if PATH corresponds to a directory. 
-*/
-static int	msh_isdir (char *path)
-{
-	t_stat	sb;
-
-	return (stat (path, &sb) == 0 && S_ISDIR (sb.st_mode));
-}
-
-static inline void	msh_canonpath_backtrack(char **p, char **q, char *base)
-{
-	(*p) += 2;
-	if ((*q) > base)
-	{
-		while (--(*q)  > base && (*q)[0] != '/')
-			;
-	}
-}
-static inline void	msh_canonpath_cpy(char **p, char **q, char *base)
-{
-	if ((*q)  != base)
-		*(*q) ++ = '/';
-	while ((*p)[0] && (*p)[0] != '/')
-		*(*q) ++ = *(*p)++;
-}
-
-/*
-** Return the canonical path from PATH:
-** - Multiple '/'s are collapsed to a single '/'.
-** - Leading './'s and trailing '/.'s are removed.
-** - Trailing '/'s are removed.
-** - Non-leading '../'s and trailing '..'s are handled by removing
-**   portions of the path.
-** Variables:
-** - base ust past the root directory
-** - p points at beginning of path element we're considering.
-** - q points just past the last kept directory.
-**/
-char	*msh_canonpath(char *path)
-{
-	char	*result;
-	char	*base;
-	char	*p;
-	char	*q;
-
-	result = ft_strdup(path);
-	if (!result)
-		return (NULL);
-	base = result + (result[0] == '/') + (result[1] == '/');
-	p = base;
-	q = base;
-	while (p[0])
-	{
-		if (p[0] == '/')
-			p++;
-		else if(p[0] == '.' && (p[1] == '/' || p[1] == '\0'))
-			p++;
-		else if (p[0] == '.' && p[1] == '.' && (p[2] == '/' || p[2] == '\0'))
-			msh_canonpath_backtrack(&p, &q, base);
-		else
-			msh_canonpath_cpy(&p, &q, base);
-	}
-	q[0] = '\0';
-	return (result);
-}
-
-int  msh_cd(t_msh *msh, int argc, char **argv, char **env)
-{
-	char	*dst;
-	t_vec	*buff;
-
-	/* Opt handling */
-	if (ft_strcmp(argv[1], "-") < 0)
-	{
-		write(2, "msh: cd: ", 9);
-		write(2, argv[1], ft_strlen(argv[1]));
-		write(2, ": invalid option\ncd: usage: cd [dir]\n", 37);
-		return (EXIT_FAILURE);
-	}
-	/* Get dst*/
-	if (!argv[1])
-	{
-		dst = utils_env_get_param(env, "HOME", 4);
-		if (!dst)
-		{
-				write(2, "msh: cd: HOME not set\n", 22);
-				return (EXIT_FAILURE);
-		}
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 	}
 	return (EXIT_SUCCESS);
 }
@@ -188,7 +73,6 @@ int	msh_chdir(t_msh *msh, char *path, int update)
 		msh->cwd = path;
 	else
 	{
-<<<<<<< HEAD
 		free(path);
 		msh->cwd = getcwd(NULL, 0);
 	}
@@ -234,29 +118,6 @@ int	msh_check_path(char *dst, char *path)
 
 	errno = 0;
 	if (stat(path, &st))
-=======
-		dst = utils_env_get_param(env, "OLDPWD", 4);
-		if (!dst)
-		{
-				write(2, "msh: cd: OLDPWD not set\n", 24);
-				return (EXIT_FAILURE);
-		}
-	}
-	else
-		dst = argv[1];
-
-	// concatenate: ABS = CWD + DST
-	// Canonicalize CAN = canolize(ABS)
-	// Check if PATH exist
-	//	y-> use canonical CAN
-	//	n-> try with ABS
-	// Change directory: chdir()
-	// Update PWD:
-	//	- CAN: set PWD=CAn
-	//	- ABS: uset getCWD
-	
-	if (dst > MAXPATHLEN)
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 	{
 		if (errno == ENOENT || errno == ELOOP)
 			return (0);
@@ -267,7 +128,6 @@ int	msh_check_path(char *dst, char *path)
 		else
 			return (msh_print_error(MSG_CD, strerror(errno), NULL, EXIT_FAILURE));
 	}
-<<<<<<< HEAD
 	if (ft_strlen(dst) > MAXPATHLEN)
 		return (msh_print_error(MSG_CD, dst, MSG_ENAME2LONG, EXIT_FAILURE));
 	return (0);
@@ -320,61 +180,3 @@ int	msh_cd(t_msh *msh, t_exec *exec)
 	else
 		return (msh_set_path(msh, exec->tab[1]));
 }
-=======
-	/* Error Handling*/
-	buff = ft_vec_new(2 * (MAXPATHLEN + 1));
-	if (!buff)
-		msh_error(msh, ERR_MALLOC);
-	ft_strcpy();
-
-}
-
-char	*msh_chdir(t_msh *msh, char *dst)
-{
-	if (!msh->cwd)
-		msh->cwd = get_cwd(NULL, 0);
-}
-
-
-// char	*msh_canonpath(char *path)
-// {
-// 	char	*result;
-// 	char	*base;
-// 	char	*p;
-// 	char	*q;
-
-// 	result = strdup(path);
-// 	if (!result)
-// 		return (NULL);
-// 	base = result + 1;
-// 	if (result[1] == '/')
-// 		base++;
-// 	p = base;
-// 	q = base;
-// 	while (p[0])
-// 	{
-// 		if (p[0] == '/')
-// 			p++;
-// 		else if(p[0] == '.' && (p[1] == '/' || p[1] == '\0'))
-// 			p++;
-// 		else if (p[0] == '.' && p[1] == '.' && (p[2] == '/' || p[2] == '\0'))
-// 		{
-// 			p += 2;
-// 			if (q > base)
-// 			{
-// 				while (--q > base && q[0] != '/')
-// 					;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			if (q != base)
-// 				*q++ = '/';
-// 			while (p[0] && p[0] != '/')
-// 				*q++ = *p++;
-// 		}
-// 	}
-// 	q[0] = '\0';
-// 	return (result);
-// }
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
--- 10_MiniShell/src/builtins/msh_echo.c
+++ 10_MiniShell/src/builtins/msh_echo.c
@@ -3,23 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   msh_echo.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
-/*   By: namenega <namenega@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/30 14:13:56 by namenega          #+#    #+#             */
-/*   Updated: 2021/10/05 14:23:20 by namenega         ###   ########.fr       */
-=======
 /*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 14:13:56 by namenega          #+#    #+#             */
-/*   Updated: 2021/10/01 13:51:56 by tderwedu         ###   ########.fr       */
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
+/*   Updated: 2021/10/06 09:38:39 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/exec.h"
 
-<<<<<<< HEAD
 //! CHECK RETURN/EXIT VALUES
 
 static int	normal_echo(char **tab, int i)
@@ -43,28 +35,6 @@ static int	echo_multiple_n(int i, char **tab)
 		ft_putstr_fd(tab[i], 1);
 		if (tab[i + 1])
 			write(1, " ", 1);
-=======
-static void	normal_echo(char **tab, int i, t_exec *exec)
-{
-	while (tab[i])
-	{
-		ft_putstr_fd(tab[i], exec->io->fd);
-		write(exec->io->fd, " ", 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
-
-static int	echo_multiple_n(t_exec *exec, int i, char **tab)
-{
-	if (!tab[i + 1])
-		return (0);
-	i++;
-	while (tab[i])
-	{
-		ft_putstr(tab[i], exec->io->fd);
-		write(exec->io->fd, " ", 1);
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 		i++;
 	}
 	return (i);
@@ -72,31 +42,19 @@ static int	echo_multiple_n(t_exec *exec, int i, char **tab)
 
 static int	parse_n(char *s)
 {
-<<<<<<< HEAD
 	int	i;
-=======
-	int i;
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 
 	if (!s)
 		return (0);
 	i = ft_strlen(s);
 	if (i == 2)
 		return (1);
-<<<<<<< HEAD
 	else if (s[2] && s[2] == 'n')
-=======
-	else if (s[2] && s[2] == 'n')								//* echo -nnnn
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 	{
 		i = 2;
 		while (s[i] && s[i] == 'n')
 			i++;
-<<<<<<< HEAD
 		if (s[i + 1] && s[i + 1] != 'n')
-=======
-		if (s[i + 1] && s[i + 1] != 'n')						//* echo -nnnnpl
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 			return (0);
 		else
 			return (1);
@@ -105,12 +63,7 @@ static int	parse_n(char *s)
 		return (0);
 }
 
-<<<<<<< HEAD
 static int	if_echo_n(char **tab, int i)
-=======
-
-static int	if_echo_n(char **tab, int i, t_exec *exec)
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 {
 	int		j;
 
@@ -122,7 +75,6 @@ static int	if_echo_n(char **tab, int i, t_exec *exec)
 			break ;
 		i++;
 	}
-<<<<<<< HEAD
 	if (j == 1)
 		i = echo_multiple_n(i, tab);
 	else
@@ -130,43 +82,24 @@ static int	if_echo_n(char **tab, int i, t_exec *exec)
 		ft_putstr_fd(tab[i], 1);
 		if (tab[i + 1])
 			write(1, " ", 1);
-=======
-	if (j == 1)													//* echo -n -n -n || echo -nnnn
-		i += echo_multiple_n(exec, i, tab);
-	else														//* echo -nfadsfas
-	{															//!WRONG
-		ft_putstr_fd(tab[i], exec->io->fd);
-		write(exec->io->fd, " ", 1);
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 		i++;
 	}
 	return (i);
 }
 
-<<<<<<< HEAD
 int	msh_echo(t_msh *msh, t_exec *exec) //TODO: add return
-=======
-void	msh_echo(t_exec *exec)
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 {
 	char	**tab;
 	int		i;
 
-<<<<<<< HEAD
 	(void)msh;
 	i = 1;
 	if (!exec->tab)
 		return (EXIT_FAILURE);
-=======
-	i = 1;
-	if (!exec->tab)
-		return ;												//* Error msg need a change
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 	tab = exec->tab;
 	if (!tab[i])
 	{
 		write(1, "\n", 1);
-<<<<<<< HEAD
 		return (EXIT_FAILURE);
 	}
 	while (tab[i])
@@ -177,15 +110,4 @@ void	msh_echo(t_exec *exec)
 			i = normal_echo(tab, i);
 	}
 	return (EXIT_SUCCESS);
-=======
-		return ;
-	}
-	while (tab[i])
-	{
-		if (i == 1 && tab[i] && !ft_strncmp(tab[i], "-n", 2))	//* echo -n
-			i += ft_echo_n(tab, i, exec);
-		else if (tab[i])										//* echo
-			normal_echo(tab, i, exec);
-	}
->>>>>>> 7fba0d37e81594fe3547fdcd7d0ff558dac266a7
 }
