3577bbed65348071ead0ae9f566f6cfff2a7dfba
tderwedu <tderwedu@student.s19.be>
2021-10-26 16:25:49 +0200

    CPP08:v1.0: ex01 75% ex02 75%

--- 11_CPP/08_CPP08/ex02/MutantStack.hpp
+++ 11_CPP/08_CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: tderwedu <tderwedu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 11:38:20 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/10/26 13:40:01 by tderwedu         ###   ########.fr       */
+/*   Updated: 2021/10/26 16:10:36 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 #include <stack>
 
 template<typename T>
-class MutantStack : public Stack<T>
+class MutantStack : public std::stack<T>
 {
 	public:
-		MutantStack() {}
-		MutantStack(MutantStack const& src) { *this = rhs; }
+		MutantStack() : std::stack<T>() {}
+		MutantStack(MutantStack const& src) { *this = src; }
 		virtual ~MutantStack() {}
 
 		MutantStack&	operator=(MutantStack const& src)
@@ -29,30 +29,107 @@ class MutantStack : public Stack<T>
 				*this = src;
 			return *this;
 		}
-		class iterator : public std::iterator<std::input_iterator_tag, T>
+		class iterator
 		{
 			private:
-				std::stack<T>	curr;
+				std::stack<T>	_stk;
+				size_t			_index;
+				iterator();
 			public:
-				iterator(int* x) :p(x) {}
-				iterator(iterator const& rhs) : curr(rhs.curr) {}
-				iterator& 	operator++()
+				iterator(std::stack<T> const& stk, size_t index)
+					: _stk(stk), _index(index) {}
+				iterator(iterator const& rhs)
+					: _stk(rhs._stk), _index(rhs._index) {}
+
+				iterator& 	operator++()	{ ++_index; return *this; }
+				iterator	operator++(int)	{ iterator tmp(*this); this->operator++(); return tmp; }
+				iterator& 	operator--()	{ --_index; return *this; }
+				iterator	operator--(int)	{ iterator tmp(*this); this->operator--(); return tmp; }
+		
+				bool 		operator==(iterator const& rhs) const	{ return _index == rhs._index; }
+				bool 		operator!=(iterator const& rhs) const	{ return _index != rhs._index; }
+
+				T&			operator*()
 				{
-					++curr; // TODO
+					std::stack<T>	tmp(_stk);
+					size_t			items;
+
+					items = _stk.size() - _index;
+					while (--items)
+					{
+						tmp.push(_stk.top());
+						_stk.pop();
+					}
+					T& value = _stk.top();
+					items = tmp.size();
+					while (items--)
+					{
+						_stk.push(tmp.top());
+						tmp.pop();
+					}
+					return value;
+				}
+				iterator&	operator=(iterator const& src)
+				{
+					if (this != &src)
+					{
+						_stk = src._stk;
+						_index = src._index;
+					}
 					return *this;
 				}
-				iterator	operator++(T)
-				{
-					iterator tmp(*this);
-					this->operator++();
-					return tmp;
-				}
-				bool 		operator==(iterator const& rhs) const { return curr == rhs.p; }
-				bool 		operator!=(iterator const& rhs) const { return curr != rhs.p; }
-				T&			operator*() const { return curr.top(); }//TODO
 		};
- 	iterator			begin() { return iterator(FROM); }//TODO
-	iterator			end() { return iterator(TO >= FROM? TO+1 : TO-1); }//TODO
+
+		// class reverse_iterator
+		// {
+		// 	private:
+		// 		std::stack<T>	_stk;
+		// 		size_t			_index;
+		// 		reverse_iterator();
+		// 	public:
+		// 		reverse_iterator(std::stack<T> const& stk, size_t index)
+		// 			: _stk(stk), _index(index) {}
+		// 		reverse_iterator(reverse_iterator const& rhs)
+		// 			: _stk(rhs._stk), _index(rhs._index) {}
+
+		// 		reverse_iterator& 	operator++()	{ --_index; return *this; }
+		// 		reverse_iterator	operator++(int)	{ reverse_iterator tmp(*this); this->operator++(); return tmp; }
+		// 		reverse_iterator& 	operator--()	{ ++_index; return *this; }
+		// 		reverse_iterator	operator--(int)	{ reverse_iterator tmp(*this); this->operator--(); return tmp; }
+		
+		// 		bool 		operator==(reverse_iterator const& rhs) const	{ return _index == rhs._index; }
+		// 		bool 		operator!=(reverse_iterator const& rhs) const	{ return _index != rhs._index; }
+
+		// 		T&			operator*()
+		// 		{
+		// 			std::stack<T>	tmp(_stk);
+		// 			size_t			items;
+		// 			T				value;
+
+		// 			items = _stk.size() - _index;
+		// 			while (--items)
+		// 				tmp.push(_stk.pop());
+		// 			value = _stk.top();
+		// 			items = tmp.size();
+		// 			while (items--)
+		// 				_stk.push(tmp.pop());
+		// 			return value;
+		// 		}
+		// 		reverse_iterator&	operator=(reverse_iterator const& src)
+		// 		{
+		// 			if (this != &src)
+		// 			{
+		// 				_stk = src._stk;
+		// 				_index = src._index;
+		// 			}
+		// 			return *this;
+		// 		}
+		// };
+
+ 	iterator			begin()	{ return iterator(*this, 0); }
+	iterator			end()	{ return iterator(*this, this->size()); }
+ 	// reverse_iterator	rbegin(){ return reverse_iterator(*this, this->size(), this->size()); }
+	// reverse_iterator	rend()	{ return reverse_iterator(*this, this->size(), 0); }
 };
 
 #endif
--- 11_CPP/08_CPP08/ex02/main.cpp
+++ 11_CPP/08_CPP08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tderwedu <tderwedu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 11:38:52 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/10/26 11:39:39 by tderwedu         ###   ########.fr       */
+/*   Updated: 2021/10/26 15:57:28 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,25 @@
 
 int	main(void)
 {
-	;
+	MutantStack<int>	mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while(it != ite)
+	{
+		std::cout << *it << std::endl;++it;
+	}
+	std::stack<int> s(mstack);
 }
