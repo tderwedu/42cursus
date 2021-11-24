c593da4deb9d8556579ca54f4d5ee575bde2e1b7
tderwedu <tderwedu@student.s19.be>
2021-11-24 10:47:56 +0100

    ft_containers:v0.0 - 0.1%

---
+++ 13_ft_containers/iterator.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tderwedu <tderwedu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 10:09:31 by tderwedu          #+#    #+#             */
+/*   Updated: 2021/11/24 10:14:13 by tderwedu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_HPP
+# define ITERATOR_HPP
+
+template <class Category, class T, class Distance = ptrdiff_t,
+          class Pointer = T*, class Reference = T&>
+  struct iterator {
+    typedef T         value_type;
+    typedef Distance  difference_type;
+    typedef Pointer   pointer;
+    typedef Reference reference;
+    typedef Category  iterator_category;
+  };
+
+#endif
