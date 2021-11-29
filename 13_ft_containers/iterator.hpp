5fe5b8e6595e0af8a7f087552b7e7da8d86cc9d4
tderwedu <tderwedu@student.s19.be>
2021-11-29 15:31:50 +0100

    ft_containers - iterator_traits DONE

--- 13_ft_containers/iterator.hpp
+++ 13_ft_containers/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 10:09:31 by tderwedu          #+#    #+#             */
-/*   Updated: 2021/11/29 14:13:26 by tderwedu         ###   ########.fr       */
+/*   Updated: 2021/11/29 15:31:33 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@
 namespace ft
 {
 
-/* ============================ ITERATOR_TRAITS ============================ */
-
-/* ============================ REVERSE_ITERATOR ============================ */
-
+#include <iterator>
 /*
-** This base class only provides some member types needed for the default 
-** iterator_traits class template.
-**
-** - T         : Type of elements pointed by the iterator.
-** - Distance  : Type to represent the difference between two iterators.
-** - Pointer   : Type to represent a pointer to an element pointed by the iterator.
-** - Reference : Type to represent a reference to an element pointed by the iterator.
-*/
+
+########################
+# class :   ITERATOR   #
+########################
+	This base class only provides some member types needed for the default 
+	iterator_traits class template.
+- T         : Type of elements pointed by the iterator.
+- Distance  : Type to represent the difference between two iterators.
+- Pointer   : Type to represent a pointer to an element pointed by the iterator.
+- Reference : Type to represent a reference to an element pointed by the iterator.
+
 template<class Category, class T, class Distance = ptrdiff_t,
 		class Pointer = T*, class Reference = T&>
 struct iterator {
@@ -40,24 +40,40 @@ struct iterator {
 	typedef Reference	reference;
 	typedef Category	iterator_category;
 };
+*/
+/* ============================ ITERATOR_TRAITS ============================ */
 
-template <class Iterator>
-class iterator_traits
-{
-
+template<class Iterator>
+struct iterator_traits {
+	typedef Iterator::T			value_type;
+	typedef Iterator::Distance	difference_type;
+	typedef Iterator::Pointer	pointer;
+	typedef Iterator::Reference	reference;
+	typedef Iterator::Category	iterator_category;
 };
 
-template <class T> class
-iterator_traits<T*>
-{
-
+// Partial specialization for pointer types.
+template<class T>
+struct iterator_traits<T*> {
+	typedef ptrdiff_t						difference_type;
+	typedef T								value_type;
+	typedef T*								pointer;
+	typedef T&								reference;
+	typedef std::random_access_iterator_tag	iterator_category;
 };
 
-template <class T> class
-iterator_traits<const T*>
-{
-
+// Partial specialization for const pointer types.
+template<class T>
+struct iterator_traits<const T*> {
+	typedef ptrdiff_t						difference_type;
+	typedef T								value_type;
+	typedef const T*						pointer;
+	typedef const T&						reference;
+	typedef std::random_access_iterator_tag	iterator_category;
 };
+
+/* ============================ REVERSE_ITERATOR ============================ */
+
 
 }
 
