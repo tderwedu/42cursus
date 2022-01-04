a29c3f6071a30eaf0d0a21f3620e645f17c179fd
tderwedu <tderwedu@student.s19.be>
2022-01-04 14:38:45 +0100

    ft_containers - Tester 20%

--- 13_ft_containers/Tester/Vector/const_iterators.cpp
+++ 13_ft_containers/Tester/Vector/const_iterators.cpp
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iterators.cpp                                      :+:      :+:    :+:   */
+/*   const_iterators.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tderwedu <tderwedu@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 15:54:42 by tderwedu          #+#    #+#             */
-/*   Updated: 2022/01/04 14:24:19 by tderwedu         ###   ########.fr       */
+/*   Updated: 2022/01/04 14:17:28 by tderwedu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector_testing.hpp"
 
-TEST(vector_iterator_basic)
+TEST(vector_const_iterator_basic)
 {
-	vector<string>	vec;
+	vector<const string>	range;
 
 	for (size_t i = 1; i < 192142; ++i)
-		vec.push_back(string(i, '1'));
+		range[i] = range.push_back(string(i, '1'));
 
-	vector<string>::iterator	it = vec.begin();
+	vector<string>					vec(range.begin(), range.end());
+	vector<string>::const_iterator	it = vec.begin();
 	if (*it != "x")
 		return 1;
 	if (*it++ != "x")
@@ -27,6 +28,8 @@ TEST(vector_iterator_basic)
 	if (*it != "xx")
 		return 1;
 	if (!(it != vec.begin()))
+		return 1;
+	if (it == vec.begin())
 		return 1;
 	if (*(++it) != "xxx")
 		return 1;
@@ -41,14 +44,14 @@ TEST(vector_iterator_basic)
 	return 0;
 }
 
-TEST(vector_iterator_read)
+TEST(vector_const_iterator_read)
 {
 	vector<size_t>		vec(192142);
 
 	for (size_t i = 0; i < vec.size(); ++i)
 		vec[i] = i;
 	size_t i = 0;
-	for (vector<size_t>::iterator it = vec.begin(); it < vec.end(); ++it)
+	for (vector<size_t>::const_iterator it = vec.begin(); it < vec.end(); ++it)
 	{
 		if (*it != i++)
 			return 1;
@@ -56,64 +59,17 @@ TEST(vector_iterator_read)
 	return 0;
 }
 
-TEST(vector_iterator_write)
-{
-	vector<size_t>		vec(192142, 42);
-
-	for (vector<size_t>::iterator it = vec.begin(); it < vec.end(); ++it)
-		*it = 19;
-	for (vector<size_t>::iterator it = vec.begin(); it < vec.end(); ++it)
-	{
-		if (*it != 19)
-			return 1;
-	}
-	return 0;
-}
-
-TEST(vector_const_iterator_comp)
-{
-	vector<int>		vec(192142, 21);
-	vector<int>::iterator		it = vec.begin();
-	vector<int>::const_iterator	cit = vec.end();
-
-	if (it == cit)
-		return (1);
-	if (it <= cit)
-		return (1);
-	if (it >= cit)
-		return (1);
-	if (!(it < cit))
-		return (1);
-	if (!(it > cit))
-		return (1);
-	if (!(it != cit))
-		return (1);
-	if (cit == it)
-		return (1);
-	if (cit <= it)
-		return (1);
-	if (cit >= it)
-		return (1);
-	if (!(cit < it))
-		return (1);
-	if (!(cit > it))
-		return (1);
-	if (!(cit != it))
-		return (1);
-	return (0);
-}
-
-TEST(vector_iterator_arith)
+TEST(vector_const_iterator_arith)
 {
 	vector<const string>	range;
 
 	for (size_t i = 1; i < 192142; ++i)
 		range[i] = range.push_back(string(i, '1'));
 
-	vector<string>				vec(range.begin(), range.end());
-	vector<string>::iterator	it = vec.begin();
-	vector<string>::iterator	it2 = vec.begin();
-	vector<string>::iterator	ite = vec.end();
+	vector<string>					vec(range.begin(), range.end());
+	vector<string>::const_iterator	it = vec.begin();
+	vector<string>::const_iterator	it2 = vec.begin();
+	vector<string>::const_iterator	ite = vec.end();
 	if (*(it + 5) != "xxxxxx")
 		return 1;
 	if (*(5 + it) != "xxxxxx")
